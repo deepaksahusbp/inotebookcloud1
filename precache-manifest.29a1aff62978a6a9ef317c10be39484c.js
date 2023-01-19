adfce9d9e03a25442021a915886ca2a174f992a5
deepaksahusbp <deepaksahusbp@gmail.com>
2023-01-19 12:53:22 +0530

    Updates

---
+++ precache-manifest.29a1aff62978a6a9ef317c10be39484c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21ae1fbab090836d8fe9",
+    "url": "/inotebookcloud1/static/css/main.c826b047.chunk.css"
+  },
+  {
+    "revision": "21ae1fbab090836d8fe9",
+    "url": "/inotebookcloud1/static/js/main.21ae1fba.chunk.js"
+  },
+  {
+    "revision": "1290fcc12456e0e5038b",
+    "url": "/inotebookcloud1/static/js/1.1290fcc1.chunk.js"
+  },
+  {
+    "revision": "f85384e109dadc6c9207",
+    "url": "/inotebookcloud1/static/js/runtime~main.f85384e1.js"
+  },
+  {
+    "revision": "25fa4c9f31a3d1efd97771b26c6d8c1e",
+    "url": "/inotebookcloud1/static/media/deepak.25fa4c9f.jpg"
+  },
+  {
+    "revision": "c867c68bfaf0c35b5d29f1e90d11b1ae",
+    "url": "/inotebookcloud1/index.html"
+  }
+];
